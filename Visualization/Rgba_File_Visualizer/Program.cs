c2280a291089efd11e367b75a658e0b1a7b3c438
Nikola <nikola.r.totev@gmail.com>
2022-09-01 22:34:23 +0300

    Added Rgba_File_Visualizer to repo.

--- Visualization/Rgba_File_Visualizer/Program.cs
+++ Visualization/Rgba_File_Visualizer/Program.cs
@@ -11,85 +11,85 @@ namespace Rgba_File_Visualizer
     {
         static void Main(string[] args)
         {
-            static void Main(string[] args)
+            while (true)
             {
-                bool superRedEnabled = true;
-                String fileExt = "_converted_super_red";
-                while (true)
+                Console.WriteLine("Directory to process:");
+
+                String directory = Console.ReadLine();
+
+                if (Directory.Exists(directory))
                 {
-                    Console.WriteLine("Directory to process:");
+                    string[] files = Directory.GetFiles(directory);
 
-                    String directory = Console.ReadLine();
-
-                    if(Directory.Exists(directory))
+                    foreach (string file in files)
                     {
-                        string[] files = Directory.GetFiles(directory);
-
-                        foreach (string file in files)
+                        try
                         {
-                            try
+
+                            string ImageData = System.IO.File.ReadAllText(file);
+
+                            List<string> dims = ImageData.Split("D")[0].Split("=").ToList();
+                            List<string> pixels = ImageData.Split("D")[1].Split("-").ToList();
+
+                            Bitmap normalBitmap = new Bitmap(int.Parse(dims[0]), int.Parse(dims[1]));
+                            Bitmap superRedBitmap = new Bitmap(int.Parse(dims[0]), int.Parse(dims[1]));
+
+                            int counter = 0;
+                            int colCounter = 0;
+                            Console.WriteLine($"{dims[0]} x {dims[1]}");
+
+                            for (int i = 0; i < int.Parse(dims[1]); i++)
                             {
-
-                                string ImageData = System.IO.File.ReadAllText(file);
-
-                                List<string> dims = ImageData.Split("D")[0].Split("=").ToList();
-                                List<string> pixels = ImageData.Split("D")[1].Split("-").ToList();
-
-                                Bitmap normalBitmap = new Bitmap(int.Parse(dims[0]), int.Parse(dims[1]));
-                                Bitmap superRedBitmap = new Bitmap(int.Parse(dims[0]), int.Parse(dims[1]));
-
-                                int counter = 0;
-                                int colCounter = 0;
-                                Console.WriteLine($"{dims[0]} x {dims[1]}");
-
-                                for (int i = 0; i < int.Parse(dims[1]); i++)
+                                for (int j = 0; j < int.Parse(dims[0]); j++)
                                 {
-                                    for (int j = 0; j < int.Parse(dims[0]); j++)
+                                    List<string> rgba = pixels[counter].Split(" ").ToList();
+
+                                    Color pixelColor = Color.FromArgb(int.Parse(rgba[3]), int.Parse(rgba[0]),
+                                        int.Parse(rgba[1]), int.Parse(rgba[2]));
+                                    normalBitmap.SetPixel(j, i, pixelColor);
+
+                                    if (int.Parse(rgba[0]) >= 200)
                                     {
-                                        List<string> rgba = pixels[counter].Split(" ").ToList();
+                                        rgba[0] = "255";
+                                        rgba[1] = "0";
+                                        rgba[2] = "0";
 
-                                        Color pixelColor = Color.FromArgb(int.Parse(rgba[3]), int.Parse(rgba[0]),
-                                            int.Parse(rgba[1]), int.Parse(rgba[2]));
-                                        normalBitmap.SetPixel(j, i, pixelColor);
-
-                                        if (int.Parse(rgba[0]) >= 200 && superRedEnabled)
-                                        {
-                                            rgba[0] = "255";
-                                            rgba[1] = "0";
-                                            rgba[2] = "0";
-
-                                        }
-
-
-                                        if (int.Parse(rgba[3]) < 255)
-                                        {
-                                            Console.WriteLine("Low Alpha Detected");
-                                            rgba[0] = "255";
-                                            rgba[1] = "255";
-                                            rgba[2] = "0";
-                                            rgba[3] = "255";
-                                        }
-
-                                        pixelColor = Color.FromArgb(int.Parse(rgba[3]), int.Parse(rgba[0]),
-                                            int.Parse(rgba[1]), int.Parse(rgba[2]));
-                                        superRedBitmap.SetPixel(j, i, pixelColor);
-
-                                        counter++;
                                     }
-                                }
 
-                                Console.WriteLine($"{counter}");
-                                normalBitmap.Save($"{file}{fileExt}.png", ImageFormat.Png);
+
+                                    if (int.Parse(rgba[3]) < 255)
+                                    {
+                                        Console.WriteLine("Low Alpha Detected");
+                                        rgba[0] = "255";
+                                        rgba[1] = "255";
+                                        rgba[2] = "0";
+                                        rgba[3] = "255";
+                                    }
+
+                                    pixelColor = Color.FromArgb(int.Parse(rgba[3]), int.Parse(rgba[0]),
+                                        int.Parse(rgba[1]), int.Parse(rgba[2]));
+                                    superRedBitmap.SetPixel(j, i, pixelColor);
+
+                                    counter++;
+                                }
                             }
-                            catch (FileNotFoundException e)
-                            {
-                                Console.WriteLine("Failed to find file.");
-                            }
+
+                            Console.WriteLine($"{counter}");
+                            normalBitmap.Save($"{file}_converted.png", ImageFormat.Png);
+                            normalBitmap.Save($"{file}_converted_super_red.png", ImageFormat.Png);
+                        }
+                        catch (FileNotFoundException e)
+                        {
+                            Console.WriteLine("Failed to find file.");
                         }
                     }
-                    
+                }
+                else
+                {
+                    Console.WriteLine("Directory does not exist!");
                 }
             }
         }
     }
 }
+
